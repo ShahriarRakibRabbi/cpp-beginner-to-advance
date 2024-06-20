d9d8f9351094e5a46042b112fb92ec7208391cb9
Md. Shahriar Rakib Rabbi <beingshahriarrakib@gmail.com>
2024-06-20 14:06:16 +0600

    boolalpha stream manipulator use

---
+++ Beginner/3. Stream Manipulation/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(void) {
+
+    cout << left << setw(15) << "Course" << left << setw(10) << "Students" << endl;
+
+    cout << left << setw(15) << "C++" << right << setw(10) << 100 << endl
+         << left << setw(15) <<"JavaScript" << right << setw(10) << 50 << endl;
+
+    //Show boolean as true or false instead of 0 and 1
+
+    bool isTrue = false;
+
+    cout << boolalpha << isTrue << endl
+         << noboolalpha << isTrue << endl;
+    
+
+    return 0;
+}
