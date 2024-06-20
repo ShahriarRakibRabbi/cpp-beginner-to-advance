5a2d4ea4f7d2b6d1da94a203c18e9d6a3fefd21d
Md. Shahriar Rakib Rabbi <beingshahriarrakib@gmail.com>
2024-06-20 14:30:01 +0600

    reading line from user in cpp

---
+++ Beginner/5. Reading Strings/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    string street;
+    string city;
+    string state;
+    string zipCode;
+
+    cout << "Enter the name of your street : " ;
+    getline(cin, street);
+
+    cout << "Enter the name of your city : " ;
+    getline(cin, city);
+
+    cout << "Enter the name of your state : " ;
+    getline(cin, state);
+
+    cout << "Enter your zipCode : " ;
+    getline(cin, zipCode);
+    
+    cout << endl << street << endl << city << ", " << state << ", " << zipCode;
+
+}
