8e6de3a4d812358aac57efc86dba65d25e63f46a
Md. Shahriar Rakib Rabbi <beingshahriarrakib@gmail.com>
2024-07-06 19:49:13 +0600

    Update example for bubble sort algorithm

--- Intermediate/Bubble Sort Algorithm/main.cpp
+++ Intermediate/Bubble Sort Algorithm/main.cpp
@@ -2,21 +2,26 @@
 
 
 using namespace std;
+void swap (int numbers[], int i, int j) {
+    int temp = numbers[i];
+    numbers[i] = numbers[j];
+    numbers[j] = temp;
+}
+void sort(int numbers[], int size) {
+    for (int pass = 0; pass < size ; pass++) {
+        for(int i = 1; i < size; i++) {
+                if(numbers[i - 1] > numbers[i])
+                    swap(numbers, i, (i - 1));
+            }
+    }
+}
 
 int main(void) {
     int numbers[] = { 3, 4, 5, 8, 1, 9, 2, 0 };
 
-    for(int i = 0; i < size(numbers) - 1; i++) {
-        for(int j = 0; j < size(numbers) - (i+1); j++) {
-            if(numbers[j] > numbers[j+1]){
-                int temp = numbers[j];
-                numbers[j] = numbers[j+1];
-                numbers[j+1] = temp;
-            }
-        }
-    }
-
-    for(int number: numbers){
+    sort(numbers, size(numbers));
+    for (int number: numbers)
         cout << number << endl;
-    }
+
+    return 0;
 }
