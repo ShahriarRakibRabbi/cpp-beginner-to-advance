da6d136e69add36fa8a965b5da0c73444c17ac22
Md. Shahriar Rakib Rabbi <beingshahriarrakib@gmail.com>
2024-07-06 18:55:29 +0600

    Update example for linear search

--- Intermediate/01. Linear Search Algorithm/main.cpp
+++ Intermediate/01. Linear Search Algorithm/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <bits/stdc++.h>
 #include "Utils/search.hpp"
 
 
@@ -8,10 +9,9 @@ int main(void) {
 
     int number = 5;
     int numbers[] = { 1, 2, 3, 4, 5, 6 };
-    int size = sizeof(numbers) / sizeof(int);
     bool isIncluded = true;
     
-    if(search(numbers, size, number) == -1)
+    if(search(numbers, size(numbers), number) == -1)
         isIncluded = false;
         
     cout << boolalpha << isIncluded << endl;
