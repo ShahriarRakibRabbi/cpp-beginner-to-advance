2bcf2c991b96f86ddc0bf7c8ace324a27cb81387
Md. Shahriar Rakib Rabbi <beingshahriarrakib@gmail.com>
2024-08-17 00:35:22 +0600

    update example for string extraction

--- Intermediate/06. String Extraction/main.cpp
+++ Intermediate/06. String Extraction/main.cpp
@@ -5,9 +5,9 @@ using namespace std;
 
 int main(void) {
     string fullName = "Shahriar Rakib Rabbi";
-    string firstName = fullName.substr(0, 8);
-    string middleName = fullName.substr(9, 5);
-    string lastName = fullName.substr(15, 5);
+    string firstName = fullName.substr(0, fullName.find(" "));
+    string middleName = fullName.substr(fullName.find(" ") + 1, (fullName.rfind(" ") - fullName.find(" ")));
+    string lastName = fullName.substr(fullName.rfind(" ") + 1);
     cout << firstName << endl << middleName << endl << lastName;
     return 0;
 }
