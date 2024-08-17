a7d851dde0df0737c7ba1480a9b28f5c489f2c5f
Md. Shahriar Rakib Rabbi <beingshahriarrakib@gmail.com>
2024-08-18 00:48:31 +0600

    Add initial example of structure

---
+++ Intermediate/08. Structure/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+struct CustomerInfo {
+    int id;
+    string name;
+    string email;
+};
+
+int main(void) {
+    CustomerInfo customerInfo;
+    cout << "Enter the id of the customer :" ;
+    cin >> customerInfo.id;
+    cout << "Enter the name of the customer :" ;
+    cin >> customerInfo.name;
+    cout << "Enter the email of the customer :" ;
+    cin >> customerInfo.email;
+    
+    cout << endl << "=== Customer Information ===" << endl << endl;
+    cout << "ID : " << customerInfo.id << endl
+         << "Name : " << customerInfo.name << endl
+         << "E-Mail : " << customerInfo.email << endl;
+
+    return 0;
+}
