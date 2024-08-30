51870d77bbe076e1ab2d0012b3394bc492c2f404
Md. Shahriar Rakib Rabbi <beingshahriarrakib@gmail.com>
2024-08-30 07:00:48 +0600

    Update example for DateTime class

--- 03. Advanced/06. DateTime/main.cpp
+++ 03. Advanced/06. DateTime/main.cpp
@@ -4,7 +4,11 @@
 using namespace std;
 
 int main(void) {
-    DateTime dateTime{2020, 10, 10, 10, 10, 10};
-    dateTime.print();
+    try{
+        DateTime dateTime{2020, 10, 10, 10, 10, 10};
+        dateTime.print();
+    } catch (const invalid_argument& e) {
+        cout << e.what() << endl;
+    }
     return 0;
 }
